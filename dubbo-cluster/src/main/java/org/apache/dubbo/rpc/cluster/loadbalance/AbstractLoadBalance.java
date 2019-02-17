1fadcf047bd30e5e873335a9a150e10f0fce8a68
Rudy <frankersun@qq.com>
2019-02-17 14:41:30 +0800

    负载均衡基类

--- dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/AbstractLoadBalance.java
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/AbstractLoadBalance.java
@@ -26,12 +26,15 @@ import org.apache.dubbo.rpc.cluster.LoadBalance;
 import java.util.List;
 
 /**
+ * 负载均衡抽象类
  * AbstractLoadBalance
+ * <a href="http://dubbo.apache.org/zh-cn/docs/source_code_guide/loadbalance.html">官方文档</a>
  */
 public abstract class AbstractLoadBalance implements LoadBalance {
     /**
      * Calculate the weight according to the uptime proportion of warmup time
      * the new weight will be within 1(inclusive) to weight(inclusive)
+     * 重新计算服务权重
      *
      * @param uptime the uptime in milliseconds
      * @param warmup the warmup time in milliseconds
@@ -40,6 +43,7 @@ public abstract class AbstractLoadBalance implements LoadBalance {
      */
     static int calculateWarmupWeight(int uptime, int warmup, int weight) {
         int ww = (int) ((float) uptime / ((float) warmup / (float) weight));
+        // 随着服务运行时间 uptime 增大，权重计算值 ww 会慢慢接近配置值 weight
         return ww < 1 ? 1 : (ww > weight ? weight : ww);
     }
 
@@ -48,9 +52,11 @@ public abstract class AbstractLoadBalance implements LoadBalance {
         if (CollectionUtils.isEmpty(invokers)) {
             return null;
         }
+        // 如果 invokers 列表中仅有一个 Invoker，直接返回即可，无需进行负载均衡
         if (invokers.size() == 1) {
             return invokers.get(0);
         }
+        // 调用 doSelect 方法进行负载均衡，该方法为抽象方法，由子类实现
         return doSelect(invokers, url, invocation);
     }
 
@@ -60,19 +66,26 @@ public abstract class AbstractLoadBalance implements LoadBalance {
     /**
      * Get the weight of the invoker's invocation which takes warmup time into account
      * if the uptime is within the warmup time, the weight will be reduce proportionally
+     * 服务提供者权重计算逻辑
      *
      * @param invoker    the invoker
      * @param invocation the invocation of this invoker
      * @return weight
      */
     protected int getWeight(Invoker<?> invoker, Invocation invocation) {
+        // 从 url 中获取权重 weight 配置值
         int weight = invoker.getUrl().getMethodParameter(invocation.getMethodName(), Constants.WEIGHT_KEY, Constants.DEFAULT_WEIGHT);
         if (weight > 0) {
+            // 获取服务提供者启动时间戳
             long timestamp = invoker.getUrl().getParameter(Constants.REMOTE_TIMESTAMP_KEY, 0L);
             if (timestamp > 0L) {
+                // 计算服务提供者运行时长
                 int uptime = (int) (System.currentTimeMillis() - timestamp);
+                // 获取服务预热时间，默认为10分钟
                 int warmup = invoker.getUrl().getParameter(Constants.WARMUP_KEY, Constants.DEFAULT_WARMUP);
+                // 如果服务运行时间小于预热时间，则重新计算服务权重，即降权 -- 避免让服务在启动之初就处于高负载状态
                 if (uptime > 0 && uptime < warmup) {
+                    // 重新计算服务权重
                     weight = calculateWarmupWeight(uptime, warmup, weight);
                 }
             }
